063aa4762329d4accfb3fa7525989e51f91452a1
khiemnd5 <khiemnd5@gmail.com>
2018-01-13 10:56:55 +0700

    Commit 1: add file test 1

---
+++ src/com/khiemnd5/comparator/Test1.java
@@ -0,0 +1,10 @@
+package com.khiemnd5.comparator;
+
+/**
+ * Created by khiemnd5 on 1/13/18.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Print commit 1");
+    }
+}
