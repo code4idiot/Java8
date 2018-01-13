04eba5b7ad69c80065c8e616d8cdb2d535d36b4d
khiemnd5 <khiemnd5@gmail.com>
2018-01-13 10:57:47 +0700

    Commit 3: add print in commit 3

--- src/com/khiemnd5/comparator/Test1.java
+++ src/com/khiemnd5/comparator/Test1.java
@@ -7,5 +7,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Print commit 1");
         System.out.println("Print commit 2");
+        System.out.println("Print commit 3");
     }
 }
