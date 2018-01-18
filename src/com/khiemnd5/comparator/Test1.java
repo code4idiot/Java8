5fa2da2c9979747f76f846a61bc961c7967d2c8f
khiemnd5 <khiemnd5@gmail.com>
2018-01-19 02:41:05 +0700

    add everything

--- src/com/khiemnd5/comparator/Test1.java
+++ src/com/khiemnd5/comparator/Test1.java
@@ -11,5 +11,6 @@ public class Test1 {
         System.out.println("Print commit 4");
         System.out.println("Print commit 5");
         System.out.println("add test4");
+        System.out.println("Print everything");
     }
 }
